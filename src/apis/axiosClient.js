bb4f9a80c2fb6663bc83267a1d657ed67d0847c8
Kodeine99 <tung.nv2510@gmail.com>
2021-04-18 11:03:23 +0700

    add axios, setup API_URL, setup axiosClient

---
+++ src/apis/axiosClient.js
@@ -0,0 +1,35 @@
+// api/axiosClient.js
+import axios from 'axios';
+import queryString from 'query-string';
+
+// Setup default config for http requests here
+// Please have a look at here 'https://github.com/axios/axios#request-config' for the full list of configs
+
+const axiosClient = axios.create({
+  baseUrl: process.env.REACT_APP_API_URL,
+  headers: {
+    'content-type': 'application/json',
+  },
+  // auto transform params to url params
+  paramsSerializer: (params) => queryString.stringify(params),
+});
+
+axiosClient.interceptor.request.use(async (config) => {
+  // Handle token here...
+  return config;
+});
+
+axiosClient.interceptor.request.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  (error) => {
+    // Handle errors
+    throw error;
+  }
+);
+
+export default axiosClient;
